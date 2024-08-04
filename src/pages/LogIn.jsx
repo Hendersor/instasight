e93df609dca51a92ab13f0a69f8a1079787e9b18
Francisco Montejo <montejof1998@gmail.com>
2024-08-04 15:50:22 -0700

    new log in design done

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,26 +1,47 @@
 import { NavBar } from "../components/NavBar"
 
-export const LogIn = () => {
+const LogIn = () => {
   return (
-    <section className="w-full h-screen border-solid border-2 border-sky-500 flex flex-col ">
-        <NavBar/>
-        <form action="" className="border-solid border border-black w-4/5 h-4/5 my-0 rounded-lg mx-auto flex flex-col justify-evenly my-5">
-            <h1 className="text-3xl text-center">Log in</h1>
+    <section className="flex flex-col h-screen">
+      <NavBar />
+      <div className="flex-grow flex items-center justify-center">
+        <div className="border-2 border-gray-200 p-6 rounded-md shadow-lg bg-white">
+          <h1 className="text-center text-3xl font-bold mb-4">Log In</h1>
+          <div className="mb-4">
+            <p className="mb-1">Email</p>
+            <input
+              type="email"
+              className="w-full p-2 border border-gray-300 rounded shadow-sm"
+            />
+          </div>
 
-            <div className="flex justify-between px-5">
-                <label>Email</label>
-                <input className="border-solid border border-black rounded-md" type="email" />
-            </div>
+          <div className="mb-4">
+            <p className="mb-1">Password</p>
+            <input
+              type="password"
+              className="w-full p-2 border border-gray-300 rounded shadow-sm"
+            />
+          </div>
 
-            <div className="flex justify-between px-5">
-                <label>Password</label>
-                <input className="border-solid border border-black rounded-md" type="password" />
-            </div>
-
-            <button className=" w-24 h-9 rounded-lg self-center bg-black text-white pointer">
-                Sign in
-            </button>
-        </form>
+          <button className="w-full p-2 bg-blue-500 text-white rounded shadow-md hover:bg-blue-600">
+            Log In
+          </button>
+          <a
+            href=""
+            className="block text-center mt-4 text-blue-500 hover:underline"
+          >
+            Forgot password?
+          </a>
+          <p className="text-center mt-2 text-gray-600">
+            Dont have an account?{" "}
+            <a href="" className="text-blue-500 hover:underline">
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
+
+export {LogIn}
