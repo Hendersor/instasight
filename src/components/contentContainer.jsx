0153ddd3c72da35de190112c18f3e909216b7ea4
Francisco Montejo <montejof1998@gmail.com>
2024-08-27 19:32:42 -0700

    First commit from new HP

--- src/components/contentContainer.jsx
+++ src/components/contentContainer.jsx
@@ -2,7 +2,7 @@ import { PictureContainer } from "./pictureContainer"
 
 export const ContentContainer = () => {
   return (
-    <section className='w-full h-auto border-solid border-2 border-sky-500'>
+    <section className='w-full h-auto border-solid border-2 border-sky-400'>
         <PictureContainer/>
         <PictureContainer/>
         <PictureContainer/>
