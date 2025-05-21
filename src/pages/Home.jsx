fe09c5111dc0ab1de74eada5c7f3cc2999adcf2d
Francisco Montejo <montejof1998@gmail.com>
2025-05-20 19:58:27 -0700

    New navbar in the mobile screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import {NavBar} from '../components/NavBar'
 import { ContentContainer } from '../components/ContentContainer'
+import { TabBar } from '../components/TabBar'
 
 export const Home = () => {
   return (
     <>
         <NavBar/>
         <ContentContainer/>
+        <TabBar/>
     </>
   )
 }
