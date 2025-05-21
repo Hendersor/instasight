064cfcec628700a63026e39121171bab5eb89d21
Francisco Montejo <montejof1998@gmail.com>
2025-05-20 20:22:37 -0700

    Navbar responsive design done

--- src/components/contentContainer.jsx
+++ src/components/contentContainer.jsx
@@ -2,7 +2,7 @@ import { PictureContainer } from "./PictureContainer"
 
 export const ContentContainer = () => {
   return (
-    <section className='w-full h-auto border-solid border-2 border-sky-400'>
+    <section className='w-full h-auto'>
         <PictureContainer/>
         <PictureContainer/>
         <PictureContainer/>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,21 +1,20 @@
-import { BsInstagram } from "react-icons/bs";
-import { BsBookmark } from "react-icons/bs";
-import { AiOutlineUser } from "react-icons/ai";
+import { BsInstagram } from "react-icons/bs"
+import { BsPerson } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+import { BsFillPlusCircleFill } from "react-icons/bs";
 
 const NavBar = () => {
     return (
         <nav className="border-y h-16 flex justify-between px-5 sticky top-0 bg-white">
-            <div className="flex justify-between w-40 px-2.5">
+            <div className="flex justify-between items-center w-40 px-2.5 md:w-[200px]">
                 <BsInstagram className="w-6 h-6 self-center" />
                 <h1 className="self-center text-xl">Instasight</h1>
+                <BsSearch className="hidden md:flex w-6 h-6" />
             </div>
 
-            <div className="flex justify-between items-center w-28 px-2.5">
-                <BsBookmark className="w-5 h-5 self-center pointer"/>
-                
-                <figure className="rounded-full border-solid border border-black h-10 w-10 flex justify-center items-center pointer ">
-                    <AiOutlineUser className="w-6 h-6 self-center pointer"/>
-                </figure>
+            <div className="hidden md:flex justify-between w-40 px-2.5">
+                <BsFillPlusCircleFill className="w-6 h-6 self-center" />
+                <BsPerson className="w-6 h-6 self-center" />
             </div>
         </nav>
     )
