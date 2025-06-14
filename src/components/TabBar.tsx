0f9bc5b9a128220f2f43da292121f4ae0495444a
Francisco Montejo <montejof1998@gmail.com>
2025-06-14 08:21:05 -0700

    feature: new details in the upload image form for better user experience, the feature to upload an image is working

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -4,12 +4,22 @@ import { BsSearch } from "react-icons/bs";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { BsBookmarkFill } from "react-icons/bs";
 import { NavIcon } from "./NavIcon";
+import { useContext } from "react";
+import { ModalContext } from "../context/ModalContext";
 
-const TabBar = ():JSX.Element => {  
+const TabBar = ():JSX.Element | null => {  
+
+  const modal = useContext(ModalContext)
+
+  if(!modal) return null
+
+  const {openModal} = modal
+  
+
   return (
     <nav className="border-t-2 border-gray-200 bg-white fixed bottom-0 w-full flex justify-around p-3 md:hidden">
         <NavIcon icon={BsHouseDoorFill} to="/" />
-        <BsFillPlusCircleFill />
+        <BsFillPlusCircleFill onClick={() => openModal()}/>
         <BsSearch />
         <NavIcon icon={BsBookmarkFill} to="/bookmarks" />
         <NavIcon icon={BsPerson} to="/profile" />
